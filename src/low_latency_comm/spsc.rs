2c3a0ac0fb29aaf0a43204bd1b12a97c6ad0bf6b
UserLeeIsTired <166837591+UserLeeIsTired@users.noreply.github.com>
2025-11-29 10:31:08 +0800

    spsc comment added

--- src/low_latency_comm/spsc.rs
+++ src/low_latency_comm/spsc.rs
@@ -2,24 +2,27 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 use std::cell::UnsafeCell;
 use std::mem::MaybeUninit;
 
-use crate::low_latency_comm::receiver;
-
 use super::{receiver::Receiver, sender::Sender};
 
-const BUFFER_CAPACITY: usize = 4096;
+const BUFFER_CAPACITY: usize = 16384;
 
-// TODO: Add comment
+// The spsc queue is a thread safety, only one consumer and one producer
+
 pub struct SPSC<T> {
+    // head and tail index is like a critical section, due to data racing, we must use atomic
     buffer: [UnsafeCell<MaybeUninit<T>>; BUFFER_CAPACITY],
     head_index: AtomicUsize,
     tail_index: AtomicUsize,
 }
 
+// Since the queue may move between thread, it needs to have Send and Sync
 unsafe impl <T: Send> Send for SPSC<T> {}
 unsafe impl <T: Send> Sync for SPSC<T> {}
 
 impl <T> SPSC<T> {
     pub fn new() -> Self {
+        // The rust compile cannot guarantee generic T array is initialized 
+        // MaybeUninit should be used to initialize the buffer array
         let buffer: [UnsafeCell<MaybeUninit<T>>; BUFFER_CAPACITY] = unsafe {
             MaybeUninit::uninit().assume_init()
         };
@@ -27,7 +30,11 @@ impl <T> SPSC<T> {
         SPSC { buffer, head_index: AtomicUsize::new(0), tail_index: AtomicUsize::new(0) }
     }
     
+    // pushing item into the buffer
     pub fn try_push(&self, item: T) -> Result<(), ()> {
+        
+        // get the current index, store item, update index to index += 1
+
         let tail = self.tail_index.load(Ordering::Relaxed);
         let next_index = (tail + 1) % BUFFER_CAPACITY;    
 
@@ -45,7 +52,12 @@ impl <T> SPSC<T> {
         Ok(())
     }
 
+    // pop item from the buffer
+
     pub fn try_pop(&self) -> Result<T, ()> {
+
+        // get the current index, get item, update index to index += 1
+
         let head = self.head_index.load(Ordering::Relaxed);
         let next_index = (head + 1) % BUFFER_CAPACITY;
 
@@ -61,9 +73,12 @@ impl <T> SPSC<T> {
         }
     }
 
-    pub fn split(self) -> (Sender<T>, Receiver<T>) {
-        let core_queue = Box::into_raw(Box::new(self));
 
+    // split the SPSC queue into sender and receiver
+
+    pub fn split(self) -> (Sender<T>, Receiver<T>) {
+        // heap allocation here is acceptable since it is a one time set-up
+        let core_queue = Box::into_raw(Box::new(self));
         let sender = Sender::new(core_queue);
         let receiver = Receiver::new(core_queue);
 
