2db9d378282e15847d40204aebb0cfbf6650709c
UserLeeIsTired <166837591+UserLeeIsTired@users.noreply.github.com>
2025-12-19 08:36:03 +0800

    added new field in node

--- src/data_structure/node.rs
+++ src/data_structure/node.rs
@@ -7,6 +7,8 @@
 pub struct Node {
     pub user_ref_num: Option<u32>,
     pub quantity: Option<u32>,
+    pub price: Option<usize>,
+    pub side: Option<char>,
     pub prev_node: Option<usize>,
     pub next_node: Option<usize>,
 }
@@ -16,6 +18,8 @@ impl Node {
         Node {
             user_ref_num: None,
             quantity: None,
+            price: None,
+            side: None,
             prev_node: None,
             next_node: None,
         }
@@ -25,6 +29,8 @@ impl Node {
     pub fn nullify_node(&mut self) {
         self.user_ref_num = None;
         self.quantity = None;
+        self.price = None;
+        self.side = None;
         self.prev_node = None;
         self.next_node = None;
     }
@@ -39,6 +45,22 @@ impl Node {
         self.next_node
     }
 
+    pub fn get_user_ref_num(&self) -> Option<u32> {
+        self.user_ref_num
+    }
+
+    pub fn get_quantity(&self) -> Option<u32> {
+        self.quantity
+    }
+
+    pub fn get_price(&self) -> Option<usize> {
+        self.price
+    }
+
+    pub fn get_side(&self) -> Option<char> {
+        self.side
+    }
+
     pub fn set_prev(&mut self, node_ptr: Option<usize>) {
         self.prev_node = node_ptr;
     }
@@ -47,12 +69,19 @@ impl Node {
         self.next_node = node_ptr;
     }
 
-    pub fn get_detail(&self) -> (Option<u32>, Option<u32>) {
-        (self.user_ref_num, self.quantity)
-    } 
-
-    pub fn insert_detail(&mut self, user_ref_num: u32, quantity: u32) {
+    pub fn set_user_ref_num(&mut self, user_ref_num: u32) {
         self.user_ref_num = Some(user_ref_num);
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32){
         self.quantity = Some(quantity);
+    }
+
+    pub fn set_price(&mut self, price: usize) {
+        self.price = Some(price);
+    }
+
+    pub fn set_side(&mut self, side: char) {
+        self.side = Some(side);
     }
 }
